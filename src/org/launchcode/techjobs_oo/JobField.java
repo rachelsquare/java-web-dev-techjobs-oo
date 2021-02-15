be4e2acd85c339c47fb37c21de06955a351c6bf0
Rachel Skwerer <rachelskwerer@gmail.com>
2021-02-15 12:06:15 -0500

    third commit

--- src/org/launchcode/techjobs_oo/JobField.java
+++ src/org/launchcode/techjobs_oo/JobField.java
@@ -1,4 +1,59 @@
 package org.launchcode.techjobs_oo;
+import java.util.Objects;
 
-public class JobField {
+public abstract class JobField {
+    private int id;
+    private static int nextId = 1;
+    private String value;
+
+    public JobField(){
+        id = nextId;
+        nextId++;
+    }
+
+    public JobField(String value){
+        this();
+        this.value = value;
+    }
+
+    @Override
+    public String toString(){
+        if(!value.isEmpty()){
+            return value;
+        }
+        else {
+            return "Data not available";
+        }
+    }
+
+    @Override
+    public boolean equals(Object o){
+        if (this == o) return true;
+        if(!(o instanceof JobField)) return false;
+        JobField jobField = (JobField) o;
+        return getId() == jobField.getId();
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getId());
+    }
+
+    // Getters and Setters:
+
+    public int getId() {
+        return id;
+    }
+
+    public String getValue() {
+        return value;
+    }
+
+    public void setValue(String value) {
+        this.value = value;
+    }
+
+
+
+
 }
--- src/org/launchcode/techjobs_oo/PositionType.java
+++ src/org/launchcode/techjobs_oo/PositionType.java
@@ -1,61 +1,7 @@
 package org.launchcode.techjobs_oo;
 
-import java.util.Objects;
-
-public class PositionType {
-    private int id;
-    private static int nextId = 1;
-    private String value;
-
-    public PositionType() {
-        id = nextId;
-        nextId++;
-    }
-
+public class PositionType extends JobField{
     public PositionType(String value) {
-        this();
-        this.value = value;
-    }
-
-    // Add a custom toString() method that returns the data stored in 'value'.
-    @Override
-    public String toString() {
-        if (!value.isEmpty()){
-            return value;
-        }
-        else {
-            return "Data not available";
-        }
-    }
-
-    // Custom equals and hashCode methods. Consider two PositionType objects "equal" when
-    //  their id fields match.
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (!(o instanceof PositionType)) return false;
-        PositionType that = (PositionType) o;
-        return getId() == that.getId() && Objects.equals(getValue(), that.getValue());
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(getId(), getValue());
-    }
-
-
-    // Getters and Setters:
-
-    public int getId() {
-        return id;
-    }
-
-    public String getValue() {
-        return value;
-    }
-
-    public void setValue(String value) {
-        this.value = value;
+        super(value);
     }
 }
